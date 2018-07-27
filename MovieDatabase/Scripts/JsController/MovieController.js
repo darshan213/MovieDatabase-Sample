758dda77bf7b62d70983d1cf11174d9d7326420e
Darshan <darshanpatel213@gmail.com>
2018-07-27 00:50:43 -0500

    new change

--- MovieDatabase/Scripts/JsController/MovieController.js
+++ MovieDatabase/Scripts/JsController/MovieController.js
@@ -5,4 +5,10 @@ app.controller('MovieController', function ($scope) {
 
     $scope.input1 = 5;
     $scope.input2 = 8;
+
+    $scope.names = [
+        { name: 'Jani', country: 'Norway' },
+        { name: 'Hege', country: 'Sweden' },
+        { name: 'Kai', country: 'Denmark' }];
+
 });
