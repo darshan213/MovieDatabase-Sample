b363b53610bac3fe6bd4ebc6ec7dc491feb2bc36
darshan213 <30390119+darshan213@users.noreply.github.com>
2018-08-03 09:58:02 -0500

    added new database function

---
+++ MovieDatabase/Controllers/HttpController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieDatabase.Controllers
+{
+    public class HttpController : Controller
+    {
+        [HttpPost]
+        public JsonResult InsertMovie(string MovieName, string MovieYear, string MovieTime, string MovieLanguage, string MovieRelease, string 
+            MovieCountry, string Actors, string Directors)//string are connecting from the index.html. We are also getting this infromation when we click on the button.
+        {
+
+            Console.WriteLine(MovieName, MovieYear, MovieTime, MovieLanguage, MovieRelease, MovieCountry, Actors, Directors);
+
+            return Json("NA", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- MovieDatabase/Scripts/JsController/MovieController.js
+++ MovieDatabase/Scripts/JsController/MovieController.js
@@ -1,14 +1,39 @@
 ﻿var app = angular.module('MovieApp', []);
-app.controller('MovieController', function ($scope) {
+app.controller('MovieController', function ($scope, $http) {
     $scope.appName = "MovieApp";
     var appName1 = "Darshan";
 
     $scope.input1 = 5;
     $scope.input2 = 8;
 
-    $scope.names = [
-        { name: 'Jani', country: 'Norway' },
-        { name: 'Hege', country: 'Sweden' },
-        { name: 'Kai', country: 'Denmark' }];
+    // This Function is for http post, also by doing this function it will call the input and put it in the database that is attached.
+    $scope.InsertMovie = function () {
+        
+        $http({
+            method: "POST",//method//
+            url: "/Http/InsertMovie",// getting this http from the C# controller that we build name of the controller is HttpController.cs
+            dataType: 'json',//type of data
+            data: {
+                MovieName : $scope.moviename,// calling the ng-controller app from the index.html and each section that gives all the input.
+                MovieYear: $scope.movieyear,    
+                MovieTime: $scope.movietime,
+                MovieLanguage: $scope.movielanguage,
+                MovieRelease: $scope.movierelease,
+                MovieCountry: $scope.moviecountry,
+                Actors: $scope.actors,
+                Director: $scope.director,
+                },
+            headers: { "Content-Type": "application/json" }
+        }).then(function successCallback(response) {
+            console.log(response);
+            var data = response.data;
+           
+        }, function errorCallback(response) {
+            console.log(response);
+            $scope.NoResults = true;
+        });
+    }
+
+
 
 });
