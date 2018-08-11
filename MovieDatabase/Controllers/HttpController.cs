dd761c3eba179f16e2265568fe49f5e1a2cfa7e9
darshan213 <30390119+darshan213@users.noreply.github.com>
2018-08-11 05:56:55 -0500

    new

--- MovieDatabase/Controllers/HttpController.cs
+++ MovieDatabase/Controllers/HttpController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MovieDatabase.Service;
 
 namespace MovieDatabase.Controllers
 {
@@ -16,6 +17,14 @@ namespace MovieDatabase.Controllers
             Console.WriteLine(MovieName, MovieYear, MovieTime, MovieLanguage, MovieRelease, MovieCountry, Actors, Directors);
 
             return Json("NA", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public string CheckMovie(string MovieName)//string are connecting from the index.html. We are also getting this infromation when we click on the button.
+        {
+            DataService DataService = new DataService();
+            string message = DataService.GetMovieConfirmation(MovieName);
+            return message;
         }
     }
 }
---
+++ MovieDatabase/Service/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieDatabase.Service
+{
+    public class DataService
+    {
+        public string GetMovieConfirmation(string movie)
+        {
+            List<string> movie_list = new List<string>();
+            movie_list.Add("Inception");
+            movie_list.Add("Sultan");
+            movie_list.Add("MissionImpossible");
+
+            if (movie_list.Contains(movie))
+            {
+                return "exists";
+            }
+            else
+            {
+                return "Does not Exist";
+            }
+
+        }
+    }
+}
